eea42d3c76162dacb7a9457e35edaaead5aa78a2
Sreyasree Sasmal <sreyasree202@gmail.com>
2023-11-01 17:00:48 +0530

    Tailwindcss added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     return (
         <>
         <div className="">
-            <h1>Pokedex</h1>
+            <h1 className="">Pokedex</h1>
         </div>
         </>
         
