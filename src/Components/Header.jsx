894268620ddae18301201c5a127da7d75269001e
Sreyasree Sasmal <sreyasree202@gmail.com>
2023-11-01 14:46:35 +0530

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+
+
+function Header() {
+    return (
+        <>
+        <div className="">
+            <h1>Pokedex</h1>
+        </div>
+        </>
+        
+    )
+}
+
+export default Header
