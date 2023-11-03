2fc9ce94323b7d53a6e233ca27b803520c2063cb
Arnab Pal <arnabpalofficial2003@gmail.com>
2023-11-03 14:06:37 +0530

    feat: all pokemons added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,13 +1,30 @@
 
-
-function Card() {
-    return (
-        <div>
-            <div className="mx-4 my-5 h-80 w-60 bg-orange-200 rounded-xl outline-double shadow-xl shadow-orange-900">
-
-            </div>
+function Card(props) {
+  
+  return (
+    <div key={props.id}>
+      <div className="relative overflow-hidden transition duration-200 transform rounded-2xl border-4 border-orange-300 bg-orange-300 bg-opacity-50 hover:-translate-y-2 hover:shadow-2xl">
+        <img
+          className="object-contain w-full h-56 md:h-64 xl:h-80"
+          src={props.image}
+          alt=""
+        />
+        <div className="text-center p-4 font-mono bg-orange-300 hover:hidden">
+          {props.name}
+          <br />
+          {props.jname}
         </div>
-    )
+        <div className="font-mono absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-80 opacity-0 hover:opacity-100">
+          <p className="mb-4 text-lg font-bold text-gray-100">Mona Lisa</p>
+          <p className="text-sm tracking-wide text-gray-300">
+            Painted between 1503 and 1517, Da Vinci’s alluring portrait has been
+            dogged by two questions since the day it was made: Who’s the subject
+            and why is she smiling?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,97 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { colorArr } from "../../colors";
+import Card from "./Card";
 export const Content = () => {
-    return (
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="grid gap-6 row-gap-5 mb-8 lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2">
-          <a href="/" aria-label="View Item">
-            <div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
-              <img
-                className="object-cover w-full h-56 md:h-64 xl:h-80"
-                src="https://images.pexels.com/photos/3184311/pexels-photo-3184311.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
-                alt=""
-              />
-              <div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
-                <p className="mb-4 text-lg font-bold text-gray-100">Mona Lisa</p>
-                <p className="text-sm tracking-wide text-gray-300">
-                  Painted between 1503 and 1517, Da Vinci’s alluring portrait has
-                  been dogged by two questions since the day it was made: Who’s
-                  the subject and why is she smiling?
-                </p>
-              </div>
-            </div>
-          </a>
-          <a href="/" aria-label="View Item">
-            <div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
-              <img
-                className="object-cover w-full h-56 md:h-64 xl:h-80"
-                src="https://images.pexels.com/photos/3184338/pexels-photo-3184338.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                alt=""
-              />
-              <div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
-                <p className="mb-4 text-lg font-bold text-gray-100">
-                  The Starry Night
-                </p>
-                <p className="text-sm tracking-wide text-gray-300">
-                  Vincent Van Gogh’s most popular painting, The Starry Night was
-                  created by Van Gogh at the asylum in Saint-Rémy, where he’d
-                  committed himself in 1889.
-                </p>
-              </div>
-            </div>
-          </a>
-          <a href="/" aria-label="View Item">
-            <div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
-              <img
-                className="object-cover w-full h-56 md:h-64 xl:h-80"
-                src="https://images.pexels.com/photos/3184339/pexels-photo-3184339.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                alt=""
-              />
-              <div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
-                <p className="mb-4 text-lg font-bold text-gray-100">The Kiss</p>
-                <p className="text-sm tracking-wide text-gray-300">
-                  Opulently gilded and extravagantly patterned, The Kiss, Gustav
-                  Klimt’s fin-de-siècle portrayal of intimacy, is a mix of
-                  Symbolism and Vienna Jugendstil, the Austrian variant of Art
-                  Nouveau.
-                </p>
-              </div>
-            </div>
-          </a>
-          <a href="/" aria-label="View Item">
-            <div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
-              <img
-                className="object-cover w-full h-56 md:h-64 xl:h-80"
-                src="https://images.pexels.com/photos/3184360/pexels-photo-3184360.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                alt=""
-              />
-              <div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
-                <p className="mb-4 text-lg font-bold text-gray-100">
-                  The Harvesters
-                </p>
-                <p className="text-sm tracking-wide text-gray-300">
-                  Bruegel’s fanfare for the common man is considered one of the
-                  defining works of Western art. This composition was one of six
-                  created on the theme of the seasons.
-                </p>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div className="text-center">
-          <a
-            href="/"
-            aria-label=""
-            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-          >
-            View gallery
-            <svg
-              className="inline-block w-3 ml-2"
-              fill="currentColor"
-              viewBox="0 0 12 12"
-            >
-              <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
-            </svg>
-          </a>
-        </div>
-      </div>
-    );
+  const url = `https://raw.githubusercontent.com/reezayn/pokemon-data.json/master/pokedex.json`;
+  const [pokemonData, setData] = useState([]);
+
+  const fetchInfo = () => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((d) => setData(d));
   };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  return (
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-6 row-gap-5 mb-8 lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2">
+        {pokemonData.map((pokemon,index) => (
+          <Card name={pokemon.name.english} jname={pokemon.name.japanese} image={pokemon.image.sprite}/>
+        ))}
+      </div>
+    </div>
+  );
+};
