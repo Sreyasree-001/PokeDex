ad77b3677f616093a3547caf174f3778c730ccbb
Sreyasree Sasmal <sreyasree202@gmail.com>
2023-11-11 22:02:55 +0530

    Searchbar updated

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -19,13 +19,11 @@ export const Content = ({ item }) => {
   const recordsPerPage = 20;
   const lastIndex = currentPage * recordsPerPage;
   const firstIndex = lastIndex - recordsPerPage;
-  const records = pokemonData.slice(firstIndex, lastIndex)
+  /*let records = pokemonData.slice(firstIndex, lastIndex)*/
   const npage = Math.ceil(pokemonData.length / recordsPerPage)
   /*const numbers = [...Array(npage + 1).keys()].slice(1)*/
 
   const prePage = () => {
-    console.log(records)
-    console.log(pokemonData)
     if (currentPage !== 1) {
       return setCurrentPage(currentPage - 1)
     }
@@ -37,24 +35,20 @@ export const Content = ({ item }) => {
     }
   }
 
-  /*function changeCPage(id) {
-
-  }*/
 
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20" >
       <div className="grid gap-6 row-gap-5 mb-8 lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2">
-
+      
         {
-          records.filter((pokemon) => {
+          pokemonData.filter((pokemon) => {
             if (item == "") {
               return pokemon;
             }
             else if (pokemon.name.english.toLowerCase().includes(item)) {
               return pokemon;
             }
-          })
-
+          }).slice(firstIndex, lastIndex)
             .map((pokemon, index) => {
               return (
                 <Card
@@ -73,9 +67,11 @@ export const Content = ({ item }) => {
                   }}
                 />
               )
-            })}
+            })
+        }
+        
       </div>
-      <div className="px-2 w-32 h-8 flex flex-row m-auto font-serif text-red-950 font-medium">
+      <div className="px-2 w-32 h-8 flex flex-row m-auto mr-24 font-serif text-red-950 font-medium">
         <button className="mr-5 bg-red-200 px-2 rounded-md  hover:bg-red-900" id="btn">
           <a href="#" onClick={prePage} >Prev</a>
         </button>
